2964199e114cd23f3cc306d0f436a6dfc18ef51d
FrankRM <frank.rodriguez@entelgy.com>
2018-05-08 22:19:25 -0500

    subiendo version estable frontend

---
+++ src/app/shared/okta/auth.interceptor.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {OktaAuthService} from '@okta/okta-angular';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private oktaAuth: OktaAuthService) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    // Only add to localhost requests since Giphy's API fails when the request include a token
+    if (request.url.indexOf('localhost') > -1) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.oktaAuth.getAccessToken().accessToken}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
